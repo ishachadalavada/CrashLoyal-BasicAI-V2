af772d7f5d0973d58acbd254e81284584e146448
ichadalavada <isha.chadalavada@gmail.com>
2023-03-03 11:33:49 -0800

    main code

---
+++ Project2/Game/src/Mob.cpp
@@ -0,0 +1,257 @@
+// MIT License
+// 
+// Copyright(c) 2020 Kevin Dill
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "Mob.h"
+
+#include "Constants.h"
+#include "Game.h"
+#include "Player.h"
+
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+Mob::Mob(const iEntityStats& stats, const Vec2& pos, bool isNorth)
+    : Entity(stats, pos, isNorth)
+    , m_pWaypoint(NULL)
+{
+    assert(dynamic_cast<const iEntityStats_Mob*>(&stats) != NULL);
+}
+
+void Mob::tick(float deltaTSec)
+{
+    // Tick the entity first.  This will pick our target, and attack it if it's in range.
+    Entity::tick(deltaTSec);
+
+    // if our target isn't in range, move towards it.
+    if (!targetInRange())
+    {
+        move(deltaTSec);
+    }
+}
+
+void Mob::hideRogue() {
+    
+}
+
+bool Mob::isHidden() const
+{
+    // Project 2: This is where you should put the logic for checking if a Rogue is
+    // hidden or not.  It probably involves something related to calling Game::Get()
+    // to get the Game, then calling getPlayer() on the game to get each player, then
+    // going through all the entities on the players and... well, you can take it 
+    // from there.  Once you've implemented this function, you can use it elsewhere to
+    // change the Rogue's behavior, damage, etc.  It is also used in by the Graphics
+    // to change the way the character renders (Rogues on the South team will render
+    // as grayed our when hidden, ones on the North team won't render at all).
+
+    // TODO: This special case code for the Rogue doesn't belong in the Mob class - we
+    // need some way to encapsulate and decouple.  I'm thinking maybe a lambda in the
+    // EntityStats??
+
+    // As a placeholder, just mark Rogues as always hidden.
+    bool isRogue = strcmp(this->getStats().getName(), "Rogue") == 0;
+    if (isRogue) {
+        Player& northPlayer = Game::get().getPlayer(true);
+        Player& southPlayer = Game::get().getPlayer(false);
+        vector<Mob*> oppMobsList;
+        const vector<Entity*>& northMobs = northPlayer.getMobs();
+        const vector<Entity*>& southMobs = southPlayer.getMobs();
+        
+        if (m_bNorth) {
+            for (Entity* temp: southMobs) {
+                Mob* mobtemp = (Mob*) temp;
+                oppMobsList.push_back(mobtemp);
+            }
+        }
+        else {
+            for (Entity* temp: northMobs) {
+                Mob* mobtemp = (Mob*) temp;
+                oppMobsList.push_back(mobtemp);
+            }
+        }
+        
+        
+        
+        for (Mob* mobtemp: oppMobsList) {
+            const Vec2& omobpos = mobtemp->getPosition();
+            const Vec2& currpos = m_Pos;
+            double distX = (currpos.x - omobpos.x);
+            double distY = (currpos.y - omobpos.y);
+            double distPos = sqrt((distX * distX) + (distY * distY));
+            if (distPos < m_Stats.getSightRadius()) {
+                cout << "Rogue is seen by " << mobtemp->getStats().getName() << endl;
+                return false;
+            }
+        }
+        
+        
+        return true;
+    }
+    else {
+        return getStats().getMobType() == iEntityStats::MobType::Rogue;
+    }
+}
+
+void Mob::move(float deltaTSec)
+{
+    // Project 2: You'll likely need to do some work in this function to get the 
+    // Rogue to move correctly (i.e. hide behind Giant or Tower, move with the Giant,
+    // spring out when doing a sneak attack, etc).
+    //   Of note, putting special case code for a single type of unit into the base
+    // class function like this IS AN AWFUL IDEA, because it wouldn't scale as more
+    // types of units are added.  We only have the one special unit type, so we can 
+    // afford to be a lazy in this instance... but if this were production code, I 
+    // would never do it this way!!
+    //   TODO: Build a better system for encapsulating unit-specific logic, perhaps
+    // by repurposing and expanding the EntityStats subclasses (which need some love
+    // as well!)
+
+    // If we have a target and it's on the same side of the river, we move towards it.
+    //  Otherwise, we move toward the bridge.
+    bool bMoveToTarget = false;
+    if (!!m_pTarget)
+    {    
+        bool imTop = m_Pos.y < (GAME_GRID_HEIGHT / 2);
+        bool otherTop = m_pTarget->getPosition().y < (GAME_GRID_HEIGHT / 2);
+
+        if (imTop == otherTop)
+        {
+            bMoveToTarget = true;
+        }
+    }
+
+    Vec2 destPos;
+    if (bMoveToTarget)
+    { 
+        m_pWaypoint = NULL;
+        destPos = m_pTarget->getPosition();
+    }
+    else
+    {
+        if (!m_pWaypoint)
+        {
+            m_pWaypoint = pickWaypoint();
+        }
+        destPos = m_pWaypoint ? *m_pWaypoint : m_Pos;
+    }
+
+    // Actually do the moving
+    Vec2 moveVec = destPos - m_Pos;
+    float distRemaining = moveVec.normalize();
+    float moveDist = m_Stats.getSpeed() * deltaTSec;
+
+    // if we're moving to m_pTarget, don't move into it
+    if (bMoveToTarget)
+    {
+        assert(m_pTarget);
+        distRemaining -= (m_Stats.getSize() + m_pTarget->getStats().getSize()) / 2.f;
+        distRemaining = std::max(0.f, distRemaining);
+    }
+
+    if (moveDist <= distRemaining)
+    {
+        m_Pos += moveVec * moveDist;
+    }
+    else
+    {
+        m_Pos += moveVec * distRemaining;
+
+        // if the destination was a waypoint, find the next one and continue movement
+        if (m_pWaypoint)
+        {
+            m_pWaypoint = pickWaypoint();
+            destPos = m_pWaypoint ? *m_pWaypoint : m_Pos;
+            moveVec = destPos - m_Pos;
+            moveVec.normalize();
+            m_Pos += moveVec * distRemaining;
+        }
+    }
+
+    // Project 1: This is where your collision code will be called from
+    Mob* otherMob = checkCollision();
+    if (otherMob) {
+        processCollision(otherMob, deltaTSec);
+    }
+}
+
+const Vec2* Mob::pickWaypoint()
+{
+    // Project 2:  You may need to make some adjustments here, so that Rogues will go
+    // back to a friendly tower when they have nothing to attack or hide behind, rather
+    // than suicide-rushing the enemy tower (which they can't damage).
+    //   Again, special-case code in a base class function bad.  Encapsulation good.
+    
+    
+    float smallestDistSq = FLT_MAX;
+    const Vec2* pClosest = NULL;
+    bool isRogue = strcmp(this->getStats().getName(), "Rogue") == 0;
+    
+    for (const Vec2& pt : Game::get().getWaypoints())
+    {
+        // Filter out any waypoints that are behind (or barely in front of) us.
+        // NOTE: (0, 0) is the top left corner of the screen
+        float yOffset = pt.y - m_Pos.y;
+        if (((m_bNorth && (yOffset < 1.f)) ||
+             (!m_bNorth && (yOffset > -1.f))) && !(isRogue))
+        {
+            continue;
+        }
+        
+        else if (((!m_bNorth && (yOffset < 1.f)) ||
+                  (m_bNorth && (yOffset > -1.f))) && (isRogue)) {
+            continue;
+        }
+        
+        float distSq = m_Pos.distSqr(pt);
+        if (distSq < smallestDistSq) {
+            smallestDistSq = distSq;
+            pClosest = &pt;
+        }
+    }
+    
+    return pClosest;
+}
+
+// Project 1: 
+//  1) return a vector of mobs that we're colliding with
+//  2) handle collision with towers & river 
+Mob* Mob::checkCollision() 
+{
+    //for (const Mob* pOtherMob : Game::get().getMobs())
+    //{
+    //    if (this == pOtherMob) 
+    //    {
+    //        continue;
+    //    }
+
+    //    // Project 1: YOUR CODE CHECKING FOR A COLLISION GOES HERE
+    //}
+    return NULL;
+}
+
+void Mob::processCollision(Mob* otherMob, float deltaTSec) 
+{
+    // Project 1: YOUR COLLISION HANDLING CODE GOES HERE
+}
+
---
+++ Project2/Game/src/Mob.h
@@ -0,0 +1,48 @@
+// MIT License
+// 
+// Copyright(c) 2020 Kevin Dill
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this softwareand associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and /or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+// 
+// The above copyright noticeand this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#pragma once
+
+#include "Entity.h"
+
+struct Waypoint;
+
+class Mob : public Entity {
+
+public:
+    Mob(const iEntityStats& stats, const Vec2& pos, bool isNorth);
+
+    virtual void tick(float deltaTSec);
+    
+    void hideRogue();
+
+    virtual bool isHidden() const;
+
+protected:
+    void move(float deltaTSec);
+    const Vec2* pickWaypoint();
+    Mob* checkCollision();
+    void processCollision(Mob* otherMob, float deltaTSec);
+
+private:
+    const Vec2* m_pWaypoint;
+};
